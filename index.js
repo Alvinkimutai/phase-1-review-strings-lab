9782efaa4b06a463a20b2a79b9419d313c4cd043
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-06-25 12:58:08 +0300

    practices string concatenation and interpolation

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+'const currentUser = "Anne Wilson";'.length;
+// currentUser
+const currentUser = "Anne Wilson";
+
+// welcome message
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
+
+console.log('const currentUser = "Anne Wilson";'.length
+);
+console.log(welcomeMessage);
+//excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+//short greeting
+const shortGreeting = 'Welcome, ' + currentUser.slice(0, 1) + '!';
+
+console.log(shortGreeting);
